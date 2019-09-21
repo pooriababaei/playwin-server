22fae6333b14e6c64f20efc32a833c4d13b8d7c6
pooriya <pooriyababaei@yahoo.com>
2019-09-21 11:36:35 +0430

    bug fix

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,7 +17,7 @@ router.get('/usersCount',isAdmin, userController.usersCount);
 router.get('/',isAdmin,userController.getUsers);
 router.put('/', isUser, userController.updateUser);
 router
-    .route('/:id')
-    .get(isUserOrAdmin,userController.getUser);
+    .route(isUserOrAdmin,'/:id')
+    .get(userController.getUser);
 
 module.exports = router;
