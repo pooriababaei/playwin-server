4b5f72ec6dbc4181791e06ca8b395234ae78bbba
pooriya <pooriyababaei@yahoo.com>
2019-10-19 14:21:39 +0330

    auto reward

--- utils/onstart.js
+++ utils/onstart.js
@@ -1,3 +1,49 @@
-require('./functions').createDirectories();
+const debug = require('debug')('onStart:');
+const schedule = require('node-schedule');
+const mongoose = require('mongoose');
+const {giveRewards} = require('./dbFunctions')
+const League = mongoose.model('league');
+const Job = mongoose.model('job');
+
+if(process.env.NODE_APP_INSTANCE == 0) {
+    require('./functions').createDirectories(); 
+}
+initiateGiveRewardCronForAllLeagues();
+ async function initiateGiveRewardCronForAllLeagues() {
+    const leagues = await League.find().lean();
+    await Job.deleteMany({}).exec();
+
+    for (let league of leagues) {
+        if(league.rewarded == false) {
+            var job = new Job({
+                type:'reward',
+                property: league.collectionName,
+                fireTime: league.endTime,
+                processOwner: process.env.NODE_APP_INSTANCE != null ? process.env.NODE_APP_INSTANCE : null
+        });
+        job.save((err,job)=> {
+            if(!err) {
+                console.log('it here')
+                schedule.scheduleJob(league.collectionName,league.endTime, async function(fireTime){
+                    const jobInFireTime = await Job.findOne({property:this.name, type: 'reward'});
+                        if(jobInFireTime && Math.abs(new Date(jobInFireTime.fireTime).getTime() - fireTime.getTime()) < 1000 && (!jobInFireTime.processOwner || jobInFireTime.processOwner == process.env.NODE_APP_INSTANCE )) {
+                            console.log('it here')
+
+                            giveRewards(league.collectionName).then(newLeague=> {
+                                debug('rewarded');
+                                Job.deleteOne({property:league.collectionName, type:'reward'}).exec();
+                            }).catch(err=> {
+                                debug('error in rewarding');
+                            });
+                        }    
+                });
+            }
+        });
+        
+        }
+    }
+}
+
+
 
 
