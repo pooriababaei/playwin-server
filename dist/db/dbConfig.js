538cea3281ac120605e09d5b31ab9966838c9c4d
pooriya <pooriyababaei@yahoo.com>
2019-12-26 12:31:37 +0330

    adding type and interfaces in mongoose

--- dist/db/dbConfig.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dbUrl = 'mongodb://127.0.0.1:27017/playwin';
-//# sourceMappingURL=dbConfig.js.map
--- dist/db/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./mondb");
-//# sourceMappingURL=index.js.map
